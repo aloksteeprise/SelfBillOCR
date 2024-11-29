5e983159e7bf8a333d5358cd45f3ada8cb209d6e
harsh-08-sha <harsh.sharma@steeprise.com>
2024-11-29 15:44:54 +0530

    Aded dynamic

--- angular-ssr-master/src/app/pages/general/constant/api-constants.ts
+++ angular-ssr-master/src/app/pages/general/constant/api-constants.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'https://localhost:44337/api/',
+    //API_BASE_URL: 'https://localhost:44337/api/',
+    API_BASE_URL: 'https://wfmapi.accessfinancial.com/api/',
   };
   
