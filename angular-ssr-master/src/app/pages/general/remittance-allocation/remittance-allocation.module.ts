bd97e0d2f8686dd8ca1cd51c8019458e60a18edc
Anmol-SR <anmol.sharma@steeprise.com>
2025-03-20 13:12:20 +0530

    Remittance Changes

--- angular-ssr-master/src/app/pages/general/remittance-allocation/remittance-allocation.module.ts
+++ angular-ssr-master/src/app/pages/general/remittance-allocation/remittance-allocation.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RemittanceAllocationComponent } from './remittance-allocation.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -13,7 +12,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     imports: [
       CommonModule, 
       FormsModule, 
-      NgbModule,
+    
       MatAutocompleteModule,
       ReactiveFormsModule
 
