4bf8052220ff8d54e43eed1f7fec6a88ac35895e
anils <anil.singh@steeprise.com>
2025-03-25 14:53:34 +0530

    Update...

--- angular-ssr-master/src/app/pages/general/constant/api-constants.ts
+++ angular-ssr-master/src/app/pages/general/constant/api-constants.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-     API_BASE_URL: 'https://localhost:44337/api/',
-    //API_BASE_URL: 'https://wfmapi.accessfinancial.com/api/',
-    
+    //API_BASE_URL: 'https://localhost:44337/api/',
+    API_BASE_URL: 'https://wfmapi.accessfinancial.com/api/',
+
   };
 
 
